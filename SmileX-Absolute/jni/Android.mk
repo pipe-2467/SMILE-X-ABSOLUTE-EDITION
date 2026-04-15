6ccd54932d002fbcebabfbdaf1d09187dd347fcd
pipe-2467 <pjpipe.channel@gmail.com>
2026-04-15 09:48:34 +0700

    --

--- SmileX-Absolute/jni/Android.mk
+++ SmileX-Absolute/jni/Android.mk
@@ -1,5 +1,9 @@
 LOCAL_PATH := $(call my-dir)
+
 include $(CLEAR_VARS)
+
 LOCAL_MODULE    := SmileXCore
 LOCAL_SRC_FILES := core.cpp
+LOCAL_LDLIBS    := -llog -landroid
+
 include $(BUILD_SHARED_LIBRARY)
