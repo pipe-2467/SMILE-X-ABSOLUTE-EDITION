91facc4da0c82a7483ed53d887b9f18e3a9fc4e1
pipe-2467 <pjpipe.channel@gmail.com>
2026-04-15 09:05:30 +0700

    --

--- SmileX-Absolute/app/MainActivity.java
+++ SmileX-Absolute/app/MainActivity.java
@@ -1,1 +1,23 @@
+package com.smilex.absolute;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.net.Uri;
+import android.provider.Settings;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // ตรวจสอบสิทธิ์ Overlay
+        if (!Settings.canDrawOverlays(this)) {
+            Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                    Uri.parse("package:" + getPackageName()));
+            startActivityForResult(intent, 101);
+        } else {
+            startService(new Intent(this, FloatingService.class));
+        }
+    }
+}
 
