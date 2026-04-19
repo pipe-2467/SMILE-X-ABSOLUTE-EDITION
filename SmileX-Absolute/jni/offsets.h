d39abf378a9fa79121b6ec211338e9935a1f6fe2
pipe-2467 <pjpipe.channel@gmail.com>
2026-04-19 15:09:29 +0700

    --

--- SmileX-Absolute/jni/offsets.h
+++ SmileX-Absolute/jni/offsets.h
@@ -1,8 +1,15 @@
 #ifndef OFFSETS_H
 #define OFFSETS_H
 
-// พิกัด Address สำหรับ Version 2026
-#define IDENTITY_OFFSET 0x48
-#define LUASTATE_PTR 0x1A0
+/**
+ * SMILE-X ABSOLUTE EDITION - UPDATED: 2026-04-19
+ * สำหรับ Roblox Version ล่าสุด (ARM64-v8a)
+ * 🥬💀 BFL PARTY PROTECTION SYSTEM
+ */
+
+// พิกัดสำหรับการเข้าถึง LuaState และความปลอดภัย
+#define IDENTITY_OFFSET 0x78       // อัปเดตจาก 0x48 (โครงสร้างใหม่ปี 2026)
+#define LUASTATE_PTR    0x218      // อัปเดตจาก 0x1A0 (พิกัดพอยเตอร์ใหม่)
+#define TASKSCHEDULER   0x3B4C120  // พิกัดตัวจัดการงานของระบบ
 
 #endif
