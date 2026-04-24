d0682e28e7842a695fb2989102fccec014d61a60
pipe-2467 <pjpipe.channel@gmail.com>
2026-04-24 16:43:30 +0700

    --

--- SmileX-Absolute/jni/Application.mk
+++ SmileX-Absolute/jni/Application.mk
@@ -1,3 +1,9 @@
-APP_ABI := all
+# สร้างไฟล์สำหรับทั้งสองสถาปัตยกรรม
+APP_ABI := armeabi-v7a arm64-v8a
+
+# กำหนดเวอร์ชัน Android ขั้นต่ำ
 APP_PLATFORM := android-21
+
+# ใช้ Standard C++ ล่าสุดเพื่อให้โค้ดไม่งอแง
 APP_STL := c++_static
+APP_CPPFLAGS := -frtti -fexceptions -std=c++11
