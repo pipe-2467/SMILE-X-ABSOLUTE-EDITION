12909a7050a40a4fcffb1a8f5283756279fe47bb
pipe-2467 <pjpipe.channel@gmail.com>
2026-04-24 16:42:24 +0700

    --

--- SmileX-Absolute/jni/Android.mk
+++ SmileX-Absolute/jni/Android.mk
@@ -1,10 +1,16 @@
 LOCAL_PATH := $(call my-dir)
 
+# ล้างค่าตัวแปรเก่า
 include $(CLEAR_VARS)
 
-# ชื่ออาวุธของเรา
+# ชื่อ Library (ต้องตรงกับ System.loadLibrary("smilex"))
 LOCAL_MODULE    := smilex
+
+# ระบุไฟล์ Source Code ทั้งหมด
 LOCAL_SRC_FILES := core.cpp
+
+# เชื่อมต่อ Library ระบบ (Log และ Android)
 LOCAL_LDLIBS    := -llog -landroid
 
+# สั่งให้สร้างเป็น Shared Library (.so)
 include $(BUILD_SHARED_LIBRARY)
